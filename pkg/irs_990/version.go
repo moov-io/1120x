a8e9cafbcb48a6e993d63c214c55055d26c22b2a
Jose Teixeira <mfdeveloper508@gmail.com>
2020-10-19 11:35:06 -0400

    add 990x logic

---
+++ pkg/irs_990/version.go
@@ -0,0 +1,7 @@
+// Copyright 2020 The Moov Authors
+// Use of this source code is governed by an Apache License
+// license that can be found in the LICENSE file.
+
+package irs_990
+
+const Version = "2015v2.0"
