1b0f22c8480f848ba143e449c3e30c2b2d744ba2
Jose <mfdeveloper508@gmail.com>
2020-10-29 12:39:50 -0400

    update test

--- pkg/efile/efile_test.go
+++ pkg/efile/efile_test.go
@@ -7,6 +7,7 @@ package efile
 import (
 	"encoding/json"
 	"encoding/xml"
+	"github.com/moov-io/1120x/pkg/irs_990"
 	"github.com/stretchr/testify/assert"
 	"io/ioutil"
 	"path/filepath"
@@ -20,7 +21,10 @@ func TestIrs990TransmissionFile(t *testing.T) {
 	// 1. parse from xml data
 	transmission := &Irs990TransmissionFile{}
 
-	err = xml.Unmarshal(InputXML, transmission)
+	err = xml.Unmarshal(InputXML, &transmission.Soap)
+	assert.Equal(t, nil, err)
+
+	err = transmission.Soap.Init()
 	assert.Equal(t, nil, err)
 
 	// 2. struct to xml buf
@@ -46,4 +50,50 @@ func TestIrs990TransmissionFile(t *testing.T) {
 
 	// 6. check xml buffers
 	assert.Equal(t, xmlOrgBuf, xmlBuf)
+
+	// 7. check functions
+	_, err = newTransmission.SOAPEnvelope()
+	assert.Equal(t, nil, err)
+
+	_, err = newTransmission.SOAPAttachment()
+	assert.NotNil(t, err)
+
+	version := newTransmission.Version()
+	assert.Equal(t, "2015v2.0", version)
+}
+
+func TestUnusedStructs(t *testing.T) {
+	InputXML, err := ioutil.ReadFile(filepath.Join("..", "..", "test", "testdata", "irs990_transmission_file.xml"))
+	assert.Equal(t, nil, err)
+
+	fault := &FaultDetail{}
+	err = xml.Unmarshal(InputXML, fault)
+	assert.Equal(t, nil, err)
+	_, err = xml.Marshal(fault)
+	assert.Equal(t, nil, err)
+
+	style := &EncodingStyle{}
+	err = xml.Unmarshal(InputXML, fault)
+	assert.Equal(t, nil, err)
+	_, err = xml.Marshal(style)
+	assert.Equal(t, nil, err)
+
+	var idType MessageIdType
+	err = idType.Validate()
+	assert.NotNil(t, err)
+
+	var detail TransmitterDetail
+	err = detail.Validate()
+	assert.NotNil(t, err)
+
+	mani := TransmissionManifest{
+		SubmissionDataList: SubmissionDataListType{
+			Cnt: 1,
+			SubmissionData: []SubmissionDataType{
+				{SubmissionId: irs_990.SubmissionIdType("111")},
+			},
+		},
+	}
+	err = mani.Validate()
+	assert.NotNil(t, err)
 }
